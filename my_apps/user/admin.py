f96e9f89b2130296c3f8f1fb7070605e5b94ff10
Lee3423 <1550363865@qq.com>
2021-05-27 11:23:36 +0800

    用户 admin 更改

--- my_apps/user/admin.py
+++ my_apps/user/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserFavorite, UserMessage
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(UserProfile, UserAdmin)
 
+class UserFavoriteAdmin(admin.ModelAdmin):
+    list_display = ['user','fav_id']
+    search_fields = ['user', 'fav_id']
+
+
+class UserMessageAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'has_read']
+    search_fields = ['user', 'title', 'message']
+    list_filter = ['has_read']
+
+
+admin.site.register(UserProfile, UserAdmin)
+admin.site.register(UserMessage, UserMessageAdmin)
+admin.site.register(UserFavorite, UserFavoriteAdmin)
 
